af7f3998439067d5f10365ae2cea3508f4d8c06c
Samuel <samuelfs96@gmail.com>
2023-06-07 10:53:42 -0400

    add flowbite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -18,6 +19,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
