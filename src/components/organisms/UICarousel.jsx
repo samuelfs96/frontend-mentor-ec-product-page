957490f8fe0c76bd0790373c5825c4a68020940d
Samuel <samuelfs96@gmail.com>
2023-06-08 13:03:21 -0400

    image indicators

--- src/components/organisms/UICarousel.jsx
+++ src/components/organisms/UICarousel.jsx
@@ -1,12 +1,42 @@
 import { Carousel, Modal } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CloseButton from "../atoms/CloseButton";
+
+const customTheme = {
+  indicators: {
+    wrapper:
+      "absolute -bottom-24 max-md:-bottom-16 left-1/2 flex -translate-x-1/2 space-x-3",
+    base: "w-[75px] h-[75px] max-md:w-[50px] max-md:h-[50px] rounded-xl bg-ui-pale-orange indicators bg-contain",
+    active: {
+      on: "opacity-25 border-ui-orange border-2",
+      off: "opacity-100",
+    },
+  },
+};
 
 export default function UICarousel({ images }) {
   const [openLightBox, setOpenLightBox] = useState(false);
+
+  useEffect(() => {
+    [
+      ...document.querySelectorAll(
+        openLightBox
+          ? "#lightbox-carousel .indicators"
+          : "#main-carousel .indicators"
+      ),
+    ].forEach((item, index) => {
+      item.style.backgroundImage = `url(${images[index]})`;
+    });
+  }, [openLightBox, images]);
+
   return (
     <>
-      <Carousel slide={false} className="max-md:[&>div]:rounded-none">
+      <Carousel
+        id="main-carousel"
+        slide={false}
+        className="max-md:[&>div]:rounded-none"
+        theme={customTheme}
+      >
         {images.map((image, index) => (
           <img
             key={index}
@@ -20,18 +50,21 @@ export default function UICarousel({ images }) {
         show={openLightBox}
         size="2xl"
         onClose={() => setOpenLightBox(false)}
-        className="[&>div>div]:bg-transparent [&>div>div]:shadow-none min-h-full max-sm:[&>div>div>div]:p-0"
+        className="[&>div>div]:bg-transparent [&>div>div]:shadow-none min-h-full max-sm:[&>div>div>div]:p-0 [&>div>div]:h-[550px]"
       >
         <Modal.Body className="relative">
-          <CloseButton customColor="white" className='absolute top-0 md:right-[1.5rem] max-md:right-0 z-10' onClick={() => setOpenLightBox(false)}/>
-          <Carousel
-            slide={false}
-            className="h-[400px]"
-          >
-            {images.map((image, index) => (
-              <img key={index} alt={`img of item ${index}`} src={image} />
-            ))}
-          </Carousel>
+          <CloseButton
+            customColor="white"
+            className="absolute top-0 md:right-[1.5rem] max-md:right-0 z-10"
+            onClick={() => setOpenLightBox(false)}
+          />
+          <div className="h-[400px]">
+            <Carousel id="lightbox-carousel" slide={false} theme={customTheme}>
+              {images.map((image, index) => (
+                <img key={index} alt={`img of item ${index}`} src={image} />
+              ))}
+            </Carousel>
+          </div>
         </Modal.Body>
       </Modal>
     </>
--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -12,7 +12,7 @@ export default function Product() {
   const images = useMemo(() => [img1, img2, img3, img4], []);
   return (
     <div className="container mx-auto min-[1024px]:px-24 mt-[150px] max-md:mt-[60px]">
-      <div className="flex max-md:flex-col gap-8">
+      <div className="flex max-md:flex-col gap-8 max-md:gap-16">
         <div className={`h-[350px] md:w-1/2 max-md:w-full ${state.zindexhidden ? '-z-10' : ''}`}>
             <UICarousel images={images} />
         </div>
